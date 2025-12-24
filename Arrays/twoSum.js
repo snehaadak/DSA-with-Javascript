aa28d72e5260a49eea6f23635f9fb4a74dc7c2a3
Sneha <snehaadak321@gmail.com>
2025-12-24 22:25:09 +0530

    Arrays

--- Arrays/twoSum.js
+++ Arrays/twoSum.js
@@ -5,4 +5,18 @@
 // You can return the answer in any order.
 
 
+var twoSum = function(nums, target) {
+    let i = 0;
+    for(let j=1;j<nums.length; j++){
+        if(nums[i] + nums[j] == target){
+            console.log([i,j])
+        }
+        else{
+            i++
+        }
+    }
+};
 
+twoSum([3,2,4],6)
+twoSum([2,7,11,15], 26)
+twoSum([3,3], 6 )
